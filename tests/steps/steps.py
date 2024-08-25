7e9802cb928a6665162a4929b512279b0a879930
glebmagamedov <magamedov.gleb@gmail.com>
2024-08-25 18:33:33 +0200

    feat: implement tests for get_account endpoint docs: change "{account_id}" to "{id}" in api documentation

--- tests/steps/steps.py
+++ tests/steps/steps.py
@@ -6,19 +6,39 @@ BASE_URL = "http://localhost:5000/"
 
 @given("Account is created and added to the database")
 def step_impl(context):
-    pass  # Placeholder for account creation
+    url = BASE_URL + "accounts/"
+    data ={
+        "initial_balance": 2000,
+        "owner": "Greg"
+    }
+    response = requests.post(url, json=data)
+    json_data = response.json()
+    context.status_code = response.status_code
+    context.id = json_data["id"]
+    context.balance = json_data["balance"]
 
 
 @when('I call "{endpoint_name}" endpoint and store response')
 def step_impl(context, endpoint_name):
-    pass  # Placeholder for calling the specified endpoint and storing the response
+    url = f"{BASE_URL}accounts/{context.id}"
+    response = requests.get(url)
+    json_data = response.json()
+    context.status_code = response.status_code
+    context.received_id = json_data["id"]
+    context.received_balance = json_data["balance"]
+
+
 
 
 @then('I verify that response status code is equal to "{status_code}"')
 def step_impl(context, status_code):
-    pass  # Placeholder for verifying that the status code matches the expected status code
+    assert context.status_code == 200
+
 
 
 @then("I verify that response contains the correct account data")
 def step_impl(context):
-    pass  # Placeholder for verifying that the response contains the correct account data
+    assert context.id == context.received_id
+    assert context.balance == context.received_balance
+
+
